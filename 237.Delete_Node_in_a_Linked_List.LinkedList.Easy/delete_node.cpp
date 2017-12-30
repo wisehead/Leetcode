e8f14a7768382b0eea65bb9ad82b86f4835fab17
wisehead <alex.chenhui@gmail.com>
2017-12-30 11:35:16 +0800

    237.Delete_Node_in_a_Linked_List.LinkedList.Easy

---
+++ 237.Delete_Node_in_a_Linked_List.LinkedList.Easy/delete_node.cpp
@@ -0,0 +1,30 @@
+/*******************************************************************************
+ *      file name: delete_node.cpp                                               
+ *         author: Hui Chen. (c) 17                             
+ *           mail: chenhui13@baidu.com                                        
+ *   created time: 17/12/30-12:12                                    
+ *  modified time: 17/12/30-12:12                                    
+ *******************************************************************************/
+#include <iostream>
+using namespace std;
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+struct ListNode {
+    int val;
+    ListNode *next;
+    ListNode(int x) : val(x), next(NULL) {}
+};
+class Solution {
+public:
+    void deleteNode(ListNode* node) {
+		*node = *(node->next);
+    }
+};
+int main()
+{}
