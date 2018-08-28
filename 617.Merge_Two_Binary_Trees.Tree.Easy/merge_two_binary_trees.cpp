6b22425b49aac7a8ff5b59baa2064efbff065b91
wisehead <alex.chenhui@gmail.com>
2018-08-28 17:31:37 +0800

    617.Merge_Two_Binary_Trees.Tree.Easy

---
+++ 617.Merge_Two_Binary_Trees.Tree.Easy/merge_two_binary_trees.cpp
@@ -0,0 +1,39 @@
+/*******************************************************************************
+ *      file name: merge_two_binary_trees.cpp                                               
+ *         author: Hui Chen. (c) 2018                             
+ *           mail: chenhui13@baidu.com                                        
+ *   created time: 2018/08/28-17:19:08                              
+ *  modified time: 2018/08/28-17:19:08                              
+ *******************************************************************************/
+#include <iostream>
+using namespace std;
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+};
+class Solution {
+public:
+    TreeNode* mergeTrees(TreeNode* t1, TreeNode* t2) {
+		if (!t1)
+			return t2;
+		if (!t2)
+			return t1;
+		t1->val += t2->val;
+		t1->left = mergeTrees(t1->left, t2->left);
+		t1->right = mergeTrees(t1->right, t2->right);
+		return t1;
+    }
+};
+int main()
+{}
