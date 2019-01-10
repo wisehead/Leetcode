46978890ab83fc7f68e482e1279b2fb7b79fad9b
wisehead <alex.chenhui@gmail.com>
2019-01-10 23:17:41 +0800

    888.Fair_Candy_Swap.Array.Easy/fair_candy_swap.best_solution.cpp

--- 888.Fair_Candy_Swap.Array.Easy/fair_candy_swap.best_solution.cpp
+++ 888.Fair_Candy_Swap.Array.Easy/fair_candy_swap.best_solution.cpp
@@ -15,17 +15,12 @@ using namespace std;
 class Solution {
 public:
     vector<int> fairCandySwap(vector<int> A, vector<int> B) {
-        vector<int> res;
         int dif = (accumulate(A.begin(), A.end(), 0) - accumulate(B.begin(), B.end(), 0)) / 2;
         unordered_set<int> S(A.begin(), A.end());
         for (int b: B)
             if (S.count(b + dif))
-            {
-                res.push_back(b+dif);
-                res.push_back(b);
-                return res; 
-            }
-    }    
+                return {b + dif, b};
+    }
 };
 int main()
 {}
