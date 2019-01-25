9e960c58d804f8273aa8f4ecd1cd9a1fa41bf95a
wisehead <alex.chenhui@gmail.com>
2019-01-25 12:36:44 +0800

    929.Unique_Email_Addresses.String.Easy/

---
+++ 929.Unique_Email_Addresses.String.Easy/unique_email_addresses.cpp
@@ -0,0 +1,58 @@
+/*******************************************************************************
+ *      file name: unique_email_addresses.cpp                                               
+ *         author: Hui Chen. (c) 2019                             
+ *           mail: chenhui13@baidu.com                                        
+ *   created time: 2019/01/25-10:18:23                              
+ *  modified time: 2019/01/25-10:18:23                              
+ *******************************************************************************/
+#include <iostream>
+#include <vector>
+#include <set>
+using namespace std;
+class Solution {
+public:
+    int numUniqueEmails(vector<string>& emails) {
+		int size = emails.size();
+		set<string> ss;
+		for (int i = 0; i < size; i++)
+		{
+			string temp = emails[i];
+			cout<<"emails["<<i<<"]"<<emails[i]<<endl;
+			string::iterator it;
+			for (it = temp.begin(); it != temp.end();)
+			{
+				cout<<"temp:"<<temp<<endl;
+				if (*it == '.')
+				{
+					temp.erase(it);
+					it--;
+				}
+				else if (*it == '+')
+				{
+					cout<<"*it:"<<*it<<endl;
+					cout<<"temp:"<<temp<<endl;
+					while(*it != '@')
+					{
+						cout<<"in while *it:"<<*it<<endl;
+						temp.erase(it);
+						cout<<"in while temp:"<<temp<<endl;
+					}
+					break;
+				}
+
+				it++;
+			}
+			cout<<"emails["<<i<<"]"<<temp<<endl;
+			ss.insert(temp);
+		}
+		return ss.size();
+    }
+};
+int main()
+{
+	string arr[] = {"test.email+alex@leetcode.com","test.e.mail+bob.cathy@leetcode.com","testemail+david@lee.tcode.com"};
+	vector<string> vec(arr, arr+3);
+	Solution sol;
+	int ret = sol.numUniqueEmails(vec);
+	cout<<"ret:"<<ret<<endl;
+}
