2f3654b91e0f05b6c303475a35256e9b08735924
wisehead <alex.chenhui@gmail.com>
2019-02-01 09:29:11 +0800

    944.Delete_Columns_to_Make_Sorted.Greedy.Easy/

---
+++ 944.Delete_Columns_to_Make_Sorted.Greedy.Easy/delete_columns_to_make_sorted.cpp
@@ -0,0 +1,31 @@
+/*******************************************************************************
+ *      file name: delete_columns_to_make_sorted.cpp                                               
+ *         author: Hui Chen. (c) 2019                             
+ *           mail: chenhui13@baidu.com                                        
+ *   created time: 2019/02/01-09:19:19                              
+ *  modified time: 2019/02/01-09:19:19                              
+ *******************************************************************************/
+#include <iostream>
+#include <vector>
+using namespace std;
+class Solution {
+public:
+    int minDeletionSize(vector<string>& A) {
+		int size = A.size();
+		int count = 0;
+		for (int i = 0; i < A[0].length(); i++)
+		{
+			for (int j = 0; j < size - 1; j++)
+			{
+				if (A[j+1][i] < A[j][i])
+				{
+					count++;
+					break;
+				}
+			}
+		}
+		return count;
+    }
+};
+int main()
+{}
