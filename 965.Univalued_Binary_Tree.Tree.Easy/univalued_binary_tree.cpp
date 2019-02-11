bde4d1ace747129cee95f345f11c0881a2f907dc
wisehead <alex.chenhui@gmail.com>
2019-02-11 13:38:31 +0800

    965.Univalued_Binary_Tree.Tree.Easy/

---
+++ 965.Univalued_Binary_Tree.Tree.Easy/univalued_binary_tree.cpp
@@ -0,0 +1,41 @@
+/*******************************************************************************
+ *      file name: univalued_binary_tree.cpp                                               
+ *         author: Hui Chen. (c) 2019                             
+ *           mail: chenhui13@baidu.com                                        
+ *   created time: 2019/02/11-13:23:14                              
+ *  modified time: 2019/02/11-13:23:14                              
+ *******************************************************************************/
+#include <iostream>
+using namespace std;
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+};
+class Solution {
+public:
+    bool isUnivalTree(TreeNode* root) {
+		if (!root) return true;
+		if (root->left)
+		{
+			if (root->left->val != root->val) return false;
+		}
+		if (root->right)
+		{
+			if (root->right->val != root->val) return false;
+		}
+		return (isUnivalTree(root->left) && isUnivalTree(root->right));
+    }
+};
+int main()
+{}
