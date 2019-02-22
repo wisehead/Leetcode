10611cffaea710b310a5e6c7f8e840a3dee0ea34
wisehead <alex.chenhui@gmail.com>
2019-02-22 16:43:39 +0800

    994.Rotting_Oranges.BreadthFirstSearch.Easy/rotting_oranges.best.cpp

---
+++ 994.Rotting_Oranges.BreadthFirstSearch.Easy/rotting_oranges.best.cpp
@@ -0,0 +1,33 @@
+/*******************************************************************************
+ *      file name: rotting_oranges.cpp                                               
+ *         author: Hui Chen. (c) 2019                             
+ *           mail: chenhui13@baidu.com                                        
+ *   created time: 2019/02/22-13:31:36                              
+ *  modified time: 2019/02/22-13:31:36                              
+ *******************************************************************************/
+#include <iostream>
+#include <vector>
+#include <utility>
+using namespace std;
+class Solution {
+public:
+	int rot(vector<vector<int> >& g, int i, int j, int d) {
+	  if (i < 0 || j < 0 || i >= g.size() || j >= g[i].size() || g[i][j] != 1) return 0;
+	  g[i][j] = d + 3;
+	  return 1;
+	}
+	int orangesRotting(vector<vector<int> >& g, int d = 0, int fresh = 0) {
+	  for (auto i = 0; i < g.size(); ++i) 
+	    fresh += count_if(begin(g[i]), end(g[i]), [](int j) { return j == 1; });
+	  for (auto old_fresh = fresh; fresh > 0; ++d) {
+	    for (auto i = 0; i < g.size(); ++i)
+	      for (auto j = 0; j < g[i].size(); ++j)
+	        if (g[i][j] == d + 2)
+	          fresh -= rot(g,i+1,j,d) + rot(g,i-1,j,d) + rot(g,i,j+1,d) + rot(g,i,j-1,d);
+	    if (fresh == exchange(old_fresh, fresh)) return -1;
+	  }
+	  return d;
+	}
+};
+int main()
+{}
