ed291144a9654b18a7c5bccc53a805375919e930
wisehead <alex.chenhui@gmail.com>
2019-02-26 10:04:03 +0800

    Array/18.4Sum.Array_HashTable_TwoPointers.Medium

--- Array/18.4Sum.Array_HashTable_TwoPointers.Medium/4sum.cpp
+++ Array/18.4Sum.Array_HashTable_TwoPointers.Medium/4sum.cpp
@@ -1,30 +1,30 @@
 /*******************************************************************************
- *      file name: 3sum.cpp                                               
+ *      file name: 4sum.cpp                                               
  *         author: Hui Chen. (c) 2019                             
  *           mail: chenhui13@baidu.com                                        
- *   created time: 2019/02/24-08:46:52                              
- *  modified time: 2019/02/24-08:46:52                              
+ *   created time: 2019/02/26-09:36:14                              
+ *  modified time: 2019/02/26-09:36:14                              
  *******************************************************************************/
 #include <iostream>
 #include <vector>
-#include <set>
 #include <algorithm>
+#include <set>
 using namespace std;
 class Solution {
 public:
-	vector<vector<int> > threeSum(vector<int> &num) {
+	vector<vector<int> > threeSum(vector<int> &num, int target) {
 		vector<vector<int> > res;
-		std::sort(num.begin(), num.end());
+		//std::sort(num.begin(), num.end());
 		for (int i = 0; i < num.size(); i++) {
-			int target = -num[i];
+			int t = target - num[i];
 			int front = i + 1;
 			int back = num.size() - 1;
 			while (front < back) {
 				int sum = num[front] + num[back];
 				// Finding answer which start from number num[i]
-				if (sum < target)
+				if (sum < t)
 					front++;
-				else if (sum > target)
+				else if (sum > t)
 					back--;
 				else {
 					vector<int> triplet(3, 0);
@@ -46,6 +46,32 @@ public:
 		}
 		return res;
 	}
+
+    vector<vector<int> > fourSum(vector<int>& nums, int target) {
+		vector<vector<int> > res;
+        if (nums.size() < 4) return res;
+		set<vector<int> > st;
+		std::sort(nums.begin(), nums.end());
+		for (int i = 0; i < nums.size() - 3; i++)
+		{
+			vector<vector<int> > temp;
+			vector<int> vec(nums.begin() + i + 1, nums.end());
+			temp = threeSum(vec, target - nums[i]);
+			if (temp.size() > 0)
+			{
+				for (int k = 0; k < temp.size(); k++)
+				{
+					vector<int> v;
+					v.push_back(nums[i]);
+					v.insert(v.begin() + 1, temp[k].begin(), temp[k].end());
+					st.insert(v);
+					//res.push_back(v);
+				}
+			}
+		}
+		res.assign(st.begin(), st.end());
+		return res;
+    }
 };
 int main()
 {}
