9e507410afa34f42ce6ec8c0fd90adddef698c3a
wisehead <alex.chenhui@gmail.com>
2019-02-28 18:41:14 +0800

    Array/33.Search_in_Rotated_Sorted_Array.Array_BinarySearch.Medium better

--- Array/33.Search_in_Rotated_Sorted_Array.Array_BinarySearch.Medium/search_in_rotated_sorted_array.better.cpp
+++ Array/33.Search_in_Rotated_Sorted_Array.Array_BinarySearch.Medium/search_in_rotated_sorted_array.better.cpp
@@ -13,36 +13,15 @@ public:
     int getPos(vector<int>& nums) {
 		int size = nums.size();
 		int left = 0, right = size - 1, mid = (left + right)/2;
-		while (left <= right)
+		while (left < right)
 		{
-			if (left == right) return left;
 			mid = (left + right)/2;
-            if (mid+1<size && nums[mid+1] < nums[mid]) 
-                return mid;
-            
-            if (nums[mid] == nums[right]) 
-			{
-				cout<<"xxx left:"<<left<<"	right:"<<right<<"	mid:"<<mid<<endl;
-				return mid;
-			}
-			else if (nums[mid] > nums[right])
-			{
-				cout<<"yyy left:"<<left<<"	right:"<<right<<"	mid:"<<mid<<endl;
+			if (nums[mid] > nums[right])
 				left = mid + 1;
-			}
-			else if (nums[left] <= nums[mid]) 
-			{
-				cout<<"zzz left:"<<left<<"	right:"<<right<<"	mid:"<<mid<<endl;
-                left = mid + 1;
-			}
             else
-			{
-				cout<<"aaa left:"<<left<<"	right:"<<right<<"	mid:"<<mid<<endl;
 				right = mid;
-			}
 		}
-        //if (size == 2 && nums[1] > nums[0]) return 1;
-		return mid;
+		return left;
 
 	}
     int search(vector<int>& nums, int target) {
@@ -51,17 +30,21 @@ public:
 		int left = 0, right = size - 1, mid = (left + right)/2;
 		int pos = getPos(nums);
         cout<<"pos:"<<pos<<endl;
+		if (target < nums[pos] ) return -1;
 		if (target == nums[pos] ) return pos;
-		if (target < nums[pos] && target >= nums[left])
+        if (pos == 0)
+        {
+            //
+        }
+		else if (target >= nums[left])
 		{
 			right = pos; 
 		}
-		else if (target < nums[pos] && target <= nums[right])
+		else if (target <= nums[right])
 		{
-			left = pos + 1;
+			left = pos+1;
 		}
-		else
-			return -1;
+
 		while (left <= right)
 		{
             if (left == right)
@@ -77,10 +60,6 @@ public:
 				right = mid;
 			else
 				left = mid + 1;
-			
-			//cout<<"xxx left:"<<nums[left]<<"	right:"<<nums[right]<<"	mid:"<<nums[mid]<<endl;
-			cout<<"yyy left:"<<left<<"	right:"<<right<<"	mid:"<<mid<<endl;
-			//cout<<"yyy nums[left]:"<<nums[left]<<"	nums[right]:"<<nums[right]<<"	nums[mid]:"<<nums[mid]<<endl;
 		}
 		return -1;
     }
@@ -97,7 +76,7 @@ int main()
 	vector<int> vec(arr, arr+3);
 	*/
 	int target = 1;
-	int arr[] = {1,3};
+	int arr[] = {3,1};
 	vector<int> vec(arr, arr+2);
 	//vector<int> vec(arr, arr+7);
 	//int ret = sol.search(vec, target);
