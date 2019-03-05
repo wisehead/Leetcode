277f54c2bf146ed9b8f8282cbf2d64534f18b60e
wisehead <alex.chenhui@gmail.com>
2019-03-05 09:39:49 +0800

    Array/39.Combination_Sum.Array_Backtracing.Medium/combination_sum.better1.cpp

---
+++ Array/39.Combination_Sum.Array_Backtracing.Medium/combination_sum.better1.cpp
@@ -0,0 +1,36 @@
+/*******************************************************************************
+ *      file name: combination_sum.cpp                                               
+ *         author: Hui Chen. (c) 2019                             
+ *           mail: chenhui13@baidu.com                                        
+ *   created time: 2019/03/04-09:51:47                              
+ *  modified time: 2019/03/04-09:51:47                              
+ *******************************************************************************/
+#include <iostream>
+#include <vector>
+#include <set>
+#include <algorithm>
+using namespace std;
+class Solution {
+public:
+    std::vector<std::vector<int> > combinationSum(std::vector<int> &candidates, int target) {
+        std::sort(candidates.begin(), candidates.end());
+        std::vector<std::vector<int> > res;
+        std::vector<int> combination;
+        combinationSum(candidates, target, res, combination, 0);
+        return res;
+    }
+private:
+    void combinationSum(std::vector<int> &candidates, int target, std::vector<std::vector<int> > &res, std::vector<int> &combination, int begin) {
+        if (!target) {
+            res.push_back(combination);
+            return;
+        }
+        for (int i = begin; i != candidates.size() && target >= candidates[i]; ++i) {
+            combination.push_back(candidates[i]);
+            combinationSum(candidates, target - candidates[i], res, combination, i);
+            combination.pop_back();
+        }
+    }
+};
+int main()
+{}
