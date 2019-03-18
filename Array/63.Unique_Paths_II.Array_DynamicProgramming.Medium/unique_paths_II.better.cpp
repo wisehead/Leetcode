4f0585c2c18386a8b7243f271c6442beca43887d
wisehead <alex.chenhui@gmail.com>
2019-03-18 19:07:50 +0800

    better

---
+++ Array/63.Unique_Paths_II.Array_DynamicProgramming.Medium/unique_paths_II.better.cpp
@@ -0,0 +1,43 @@
+/*******************************************************************************
+ *      file name: unique_paths_II.cpp                                               
+ *         author: Hui Chen. (c) 2019                             
+ *           mail: chenhui13@baidu.com                                        
+ *   created time: 2019/03/18-18:11:43                              
+ *  modified time: 2019/03/18-18:11:43                              
+ *******************************************************************************/
+#include <iostream>
+#include <vector>
+using namespace std;
+class Solution {
+public:
+    int uniquePathsWithObstacles(vector<vector<int>>& obstacleGrid) {
+		int m = obstacleGrid.size();
+		int n = obstacleGrid[0].size();
+		vector<unsigned long long> vec(n, 1);
+		for (int i = 0; i < m; i++)
+		{
+			for (int j = 0; j < n; j++)
+			{
+				if (obstacleGrid[i][j] != 0)
+					vec[j] = 0;                
+                else if (i == 0)
+                {
+					if (j == 0)
+                        vec[j] = 1;
+                    else
+						vec[j] = vec[j-1];                    
+                }
+                else
+                {
+				    if (j == 0)
+				    	vec[j] = vec[j];
+			    	else 
+					    vec[j] += vec[j-1];
+                }
+			}
+		}
+		return vec[n-1];
+    }
+};
+int main()
+{}
