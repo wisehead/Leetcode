9a5808ee560cba8c7f86ae556813752a743f0870
wisehead <alex.chenhui@gmail.com>
2019-03-20 09:59:09 +0800

    better

---
+++ Array/73.Set_Matrix_Zeroes.Array.Medium/set_matrix_zeroes.better.cpp
@@ -0,0 +1,48 @@
+/*******************************************************************************
+ *      file name: set_matrix_zeroes.cpp                                               
+ *         author: Hui Chen. (c) 2019                             
+ *           mail: chenhui13@baidu.com                                        
+ *   created time: 2019/03/20-09:43:26                              
+ *  modified time: 2019/03/20-09:43:26                              
+ *******************************************************************************/
+#include <iostream>
+#include <vector>
+#include <set>
+using namespace std;
+class Solution {
+public:
+    void setZeroes(vector<vector<int>>& matrix) {
+		int m = matrix.size();
+		int n = matrix[0].size();
+		set<int> si, sj;
+		for (int i = 0; i < m; i++)
+		{
+			for (int j = 0; j < n; j++)
+			{
+				if (!matrix[i][j])
+				{
+					si.insert(i);
+					sj.insert(j);
+				}
+			}
+		}
+		for (int i = 0; i < m; i++)
+		{
+			if (si.count(i))
+			{
+				for (int j = 0; j < n ; j++)
+					matrix[i][j] = 0;
+			}
+		}
+		for (int j = 0; j < n; j++)
+		{
+			if (sj.count(j))
+			{
+				for (int i = 0; i < m ; i++)
+					matrix[i][j] = 0;
+			}
+		}
+    }
+};
+int main()
+{}
