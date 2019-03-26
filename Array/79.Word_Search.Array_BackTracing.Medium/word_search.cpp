27a769a56ff51676ca72b323e1b345564d3e514e
wisehead <alex.chenhui@gmail.com>
2019-03-26 16:54:05 +0800

    Array/79.Word_Search.Array_BackTracing.Medium

---
+++ Array/79.Word_Search.Array_BackTracing.Medium/word_search.cpp
@@ -0,0 +1,64 @@
+/*******************************************************************************
+ *      file name: word_search.cpp                                               
+ *         author: Hui Chen. (c) 2019                             
+ *           mail: chenhui13@baidu.com                                        
+ *   created time: 2019/03/26-09:34:47                              
+ *  modified time: 2019/03/26-09:34:47                              
+ *******************************************************************************/
+#include <iostream>
+#include <vector>
+using namespace std;
+class Solution {
+public:
+	bool dfs(vector<vector<char>>& board, vector<vector<bool>>& flags, int i, int j, string word)
+	{
+        //cout<<"i:"<<i<<"    j:"<<j<<"   word:"<<word<<endl;
+		int len = word.length();
+		if (!len) return true;
+		int m = board.size();
+		int n = board[0].size();
+		flags[i][j] = true;
+		string str = word.substr(1, len - 1);
+		//east
+		if (j+1 < n && !flags[i][j+1] && board[i][j+1] == word[0])
+			if (dfs(board, flags, i, j+1, str)) return true;
+		//south
+		if (i+1 < m && !flags[i+1][j] && board[i+1][j] == word[0])
+			if (dfs(board, flags, i+1, j, str)) return true;
+		//west
+		if (j-1 >= 0 && !flags[i][j-1] && board[i][j-1] == word[0])
+			if (dfs(board, flags, i, j-1, str)) return true;
+		//north
+		if  (i-1 >= 0 && !flags[i-1][j] && board[i-1][j] == word[0])
+			if (dfs(board, flags, i-1, j, str)) return true;
+        flags[i][j] = false;
+		return false;
+	}
+
+    bool exist(vector<vector<char>>& board, string word) {
+		int m = board.size();
+		int n = board[0].size();
+		vector<vector<bool>> flags; 
+
+		for (int i = 0; i < m; i++)
+		{
+			for (int j = 0; j < n; j++)
+			{
+				if (board[i][j] == word[0])
+				{
+					for (int k = 0; k < m; k++)
+					{
+						vector<bool> vec(n, false);
+						flags.push_back(vec);
+					}
+					string str = word.substr(1, word.length() - 1);
+					if (dfs(board, flags, i, j, str))
+						return true;
+				}
+			}
+		}
+		return false;
+    }
+};
+int main()
+{}
