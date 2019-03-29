d5baeb8028819b54839cb94720d91a0c31ba7eac
wisehead <alex.chenhui@gmail.com>
2019-03-29 10:19:41 +0800

    best

---
+++ Array/90.Subsets_II.Array_BackTracing.Medium/subsets_II.best.cpp
@@ -0,0 +1,36 @@
+/*******************************************************************************
+ *      file name: subsets_II.cpp                                               
+ *         author: Hui Chen. (c) 2019                             
+ *           mail: chenhui13@baidu.com                                        
+ *   created time: 2019/03/29-10:07:44                              
+ *  modified time: 2019/03/29-10:07:44                              
+ *******************************************************************************/
+#include <iostream>
+#include <vector>
+#include <set>
+#include <algorithm>
+using namespace std;
+class Solution {
+public:
+    vector<vector<int> > subsetsWithDup(vector<int> &S) {
+        vector<vector<int> > totalset = {{}};
+        sort(S.begin(),S.end());
+        for(int i=0; i<S.size();){
+            int count = 0; // num of elements are the same
+            while(count + i<S.size() && S[count+i]==S[i])  count++;
+            int previousN = totalset.size();
+            for(int k=0; k<previousN; k++){
+                vector<int> instance = totalset[k];
+                for(int j=0; j<count; j++){
+                    instance.push_back(S[i]);
+                    totalset.push_back(instance);
+                }
+            }
+            i += count;
+        }
+        return totalset;
+    }
+};
+
+int main()
+{}
