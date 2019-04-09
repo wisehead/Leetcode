2ca33f910888686839706ece48e8a85858ca20f4
wisehead <alex.chenhui@gmail.com>
2019-04-09 11:01:56 +0800

    O(logN), but not easy to understand.

--- Array/209.Minimum_Size_Subarray_Sum.Array_TwoPointers_BinarySearch.Medium/minumum_size_subarray_sum.better.On.cpp
+++ Array/209.Minimum_Size_Subarray_Sum.Array_TwoPointers_BinarySearch.Medium/minumum_size_subarray_sum.better.On.cpp
@@ -8,6 +8,11 @@
 #include <iostream>
 #include <vector>
 using namespace std;
+/*
+The O(n) solution is to use two pointers: l and r. First we move r until we get a sum >= s, 
+then we move l to the right until sum < s. In this process, store the minimum length between l and r. 
+Since each element in nums will be visited by l and r for at most once. This algorithm is of O(n) time.
+ */
 class Solution {
 public:
     int minSubArrayLen(int s, vector<int>& nums) {
