c220b25ffffa54ebea3a5769534a92531040ad6d
wisehead <alex.chenhui@gmail.com>
2019-04-11 19:56:06 +0800

    clean

---
+++ Array/228.Summary_Ranges.Array.Medium/summary_ranges.clean.cpp
@@ -0,0 +1,29 @@
+/*******************************************************************************
+ *      file name: summary_ranges.cpp                                               
+ *         author: Hui Chen. (c) 2019                             
+ *           mail: chenhui13@baidu.com                                        
+ *   created time: 2019/04/11-19:19:04                              
+ *  modified time: 2019/04/11-19:19:04                              
+ *******************************************************************************/
+#include <iostream>
+#include <vector>
+#include <string>
+using namespace std;
+class Solution {
+public:
+   vector<string> summaryRanges(vector<int>& nums) {
+    const int size_n = nums.size();
+    vector<string> res;
+    if ( 0 == size_n) return res;
+    for (int i = 0; i < size_n;) {
+        int start = i, end = i;
+        while (end + 1 < size_n && nums[end+1] == nums[end] + 1) end++;
+        if (end > start) res.push_back(to_string(nums[start]) + "->" + to_string(nums[end]));
+        else res.push_back(to_string(nums[start]));
+        i = end+1;
+    }
+    return res;
+   }
+};
+int main()
+{}
