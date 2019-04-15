f19ca81f513a5503a19ab65c4c4176d37ea37e39
wisehead <alex.chenhui@gmail.com>
2019-04-15 10:27:01 +0800

    two pointers.

---
+++ Array/238.Product_of_Array_Except_Self.Array.Medium/product_of_array_except_self.twopointers.cpp
@@ -0,0 +1,33 @@
+/*******************************************************************************
+ *      file name: product_of_array_except_self.cpp                                               
+ *         author: Hui Chen. (c) 2019                             
+ *           mail: chenhui13@baidu.com                                        
+ *   created time: 2019/04/15-09:45:19                              
+ *  modified time: 2019/04/15-09:45:19                              
+ *******************************************************************************/
+#include <iostream>
+#include <vector>
+using namespace std;
+class Solution {
+public:
+    vector<int> productExceptSelf(vector<int>& nums) {
+        int n=nums.size();
+        vector<int> fromBegin(n);
+        fromBegin[0]=1;
+        vector<int> fromLast(n);
+        fromLast[0]=1;
+        
+        for(int i=1;i<n;i++){
+            fromBegin[i]=fromBegin[i-1]*nums[i-1];
+            fromLast[i]=fromLast[i-1]*nums[n-i];
+        }
+        
+        vector<int> res(n);
+        for(int i=0;i<n;i++){
+            res[i]=fromBegin[i]*fromLast[n-1-i];
+        }
+        return res;
+    }
+};
+int main()
+{}
