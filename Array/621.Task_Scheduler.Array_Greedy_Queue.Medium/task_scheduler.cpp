b71b291b824c5fadbdfba425b857f43a7b2fa518
wisehead <alex.chenhui@gmail.com>
2019-04-28 15:52:04 +0800

    priority queue.

--- Array/621.Task_Scheduler.Array_Greedy_Queue.Medium/task_scheduler.cpp
+++ Array/621.Task_Scheduler.Array_Greedy_Queue.Medium/task_scheduler.cpp
@@ -7,75 +7,48 @@
  *******************************************************************************/
 #include <iostream>
 #include <vector>
-#include <map>
-#include <set>
+#include <unordered_map>
+#include <queue>
 using namespace std;
 class Solution {
 public:
-	void printvec(vector<char>& vec) {
-		for (auto e : vec)
-			cout<<e<<",";
-		cout<<endl;
-	}
-	bool conflict(vector<char>& vec, int n, char x) {
-		int size = vec.size();
-		for (int i = size - 1; i >= 0 && i >= size - n; i--)
-		{
-			if (vec[i] == x) return true;
-		}
-		return false;
-	}
+/*
+The idea is:
+0. To work on the same task again, CPU has to wait for time n, therefore we can think of as if there is a cycle, of time n+1, regardless whether you schedule some other task in the cycle or not.
+
+To avoid leave the CPU with limited choice of tasks and having to sit there cooling down frequently at the end, it is critical the keep the diversity of the task pool for as long as possible.
+In order to do that, we should try to schedule the CPU to always try round robin between the most popular tasks at any time.
+priority_queue<task, count>
+*/
     int leastInterval(vector<char>& tasks, int n) {
-		map<char, int> mm;
-		map<int, char> m2;
-		vector<char> vec;
-		for (auto e : tasks)
-		{
-			if (mm.count(e))
-				mm[e]++;
-			else
-				mm[e] = 1;
-		}
-		map<char, int>::iterator it;
-		multimap<int, char> m3;
-		for (it = mm.begin(); it != mm.end(); it++)
-		{
-			m3.insert(make_pair(it->second, it->first));
-		}
-		map<int,char>::reverse_iterator it3;
-
-		while (!m3.empty())
-		{
-            for (it3 = m3.rbegin(); it3 != m3.rend(); )
-            {	
-                map<int,char>::reverse_iterator it4 = it3;
-    			char task = it4->second;
-                //cout<<"task:"<<task<<endl;
-			    while (it4 != m3.rend() && conflict(vec, n, task))
-				    it4++;
-			    if (it4 != m3.rend())
-			    {
-				    vec.push_back(task);
-				    int cnt = it4->first;
-				    map<int,char>::iterator tmp_it;
-				    tmp_it = m3.find(cnt);
-				    while(tmp_it->second != task)
-				    	tmp_it++;
-			    	m3.erase(tmp_it);
-				    cnt--;
-				    if (cnt > 0)
-				    {
-					    m3.insert(make_pair(cnt, task));
-				    }
-                    break;
-			    }
-                if (++it3 == m3.rend())
-                    vec.push_back('1');
+        unordered_map<char, int> counts;
+        for (char t : tasks) {
+            counts[t]++;
+        }
+        priority_queue<pair<int, char>> pq;
+        for (pair<char, int> count : counts) {
+            pq.push(make_pair(count.second, count.first));
+        }
+        int alltime = 0;
+        int cycle = n + 1;
+        while (!pq.empty()) {
+            int time = 0;
+            vector<pair<int, char>> tmp;
+            for (int i = 0; i < cycle; i++) {
+                if (!pq.empty()) {
+                    tmp.push_back(pq.top());
+                    pq.pop();
+                    time++;
+                }
             }
-
-            //printvec(vec);
-		}
-		return vec.size();
+            for (auto t : tmp) {
+                if (--t.first) {
+                    pq.push(t);
+                }
+            }
+            alltime += !pq.empty() ? cycle : time;
+        }
+        return alltime;
     }
 };
 int main()
