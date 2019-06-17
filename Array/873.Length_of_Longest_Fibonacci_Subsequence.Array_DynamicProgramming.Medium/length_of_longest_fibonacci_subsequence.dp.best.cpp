7303112c09ec4ae98a08c6da9cdee9463fbc410d
wisehead <alex.chenhui@gmail.com>
2019-06-17 15:18:29 +0800

    dp best.

---
+++ Array/873.Length_of_Longest_Fibonacci_Subsequence.Array_DynamicProgramming.Medium/length_of_longest_fibonacci_subsequence.dp.best.cpp
@@ -0,0 +1,42 @@
+/*******************************************************************************
+ *      file name: length_of_longest_fibonacci_subsequence.cpp                                               
+ *         author: Hui Chen. (c) 2019                             
+ *           mail: chenhui13@baidu.com                                        
+ *   created time: 2019/06/17-10:00:45                              
+ *  modified time: 2019/06/17-10:00:45                              
+ *******************************************************************************/
+#include <iostream>
+#include <vector>
+#include <set>
+#include <unordered_map>
+using namespace std;
+/*
+Another solution is kind of dp.
+dp[a, b] represents the length of fibo sequence ends up with (a, b)
+Then we have dp[a, b] = (dp[b - a, a] + 1 ) or 2
+The complexity reduce to O(N^2).
+In C++/Java, I use 2D dp and index as key.
+In Python, I use value as key.
+
+Time Complexity:
+O(N^2)
+ */
+class Solution {
+public:
+    int lenLongestFibSubseq(vector<int>& A) {
+        unordered_map<int, int> m;
+        int N = A.size(), res = 0;
+        int dp[N][N];
+        for (int j = 0; j < N; ++j) {
+            m[A[j]] = j;
+            for (int i = 0; i < j; ++i) {
+                int k = m.find(A[j] - A[i]) == m.end() ? -1 : m[A[j] - A[i]];
+                dp[i][j] = (A[j] - A[i] < A[i] && k >= 0) ? dp[k][i] + 1 : 2;
+                res = max(res, dp[i][j]);
+            }
+        }
+        return res > 2 ? res : 0;
+    }
+};
+int main()
+{}
