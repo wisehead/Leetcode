205f3b4546873223c93fe21165495bb3c4a481f9
wisehead <alex.chenhui@gmail.com>
2019-06-28 09:58:13 +0800

    accepted, faster than 95.9%.

---
+++ Array/915.Partition_Array_into_Disjoint_Intervals.Array.Medium/partition_array_into_disjoint_intervals.cpp
@@ -0,0 +1,34 @@
+/*******************************************************************************
+ *      file name: partition_array_into_disjoint_intervals.cpp                                               
+ *         author: Hui Chen. (c) 2019                             
+ *           mail: chenhui13@baidu.com                                        
+ *   created time: 2019/06/28-09:43:01                              
+ *  modified time: 2019/06/28-09:43:01                              
+ *******************************************************************************/
+#include <iostream>
+#include <vector>
+using namespace std;
+class Solution {
+public:
+    int partitionDisjoint(vector<int>& A) {
+		int n = A.size();
+		vector<int> vec(n, 0);
+		int minv = A[n-1];
+		int maxv = A[0];
+		for (int i = n - 1; i > 0; i--)
+		{
+			minv = min(A[i], minv);
+			vec[i] = minv;
+		}
+
+		for (int i = 0; i < n - 1; i++)
+		{
+			maxv = max(A[i], maxv);
+			if (maxv <= vec[i+1])
+				return i+1;
+		}
+		return 0;
+    }
+};
+int main()
+{}
