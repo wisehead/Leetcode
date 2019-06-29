7d2156eacc99b028f6f58ff1c992aa88385212be
wisehead <alex.chenhui@gmail.com>
2019-06-29 10:53:02 +0800

    best

--- Array/918.Maximum_Sum_Circular_Subarray.Array.Medium/maximum_sum_circular_subarray.cpp
+++ Array/918.Maximum_Sum_Circular_Subarray.Array.Medium/maximum_sum_circular_subarray.cpp
@@ -8,26 +8,51 @@
 #include <iostream>
 #include <vector>
 using namespace std;
+/*
+Intuition
+I guess you know how to solve max subarray sum (without circular).
+If not, you can have a reference here: 53. Maximum Subarray
+
+
+Explanation
+So there are two case.
+
+The first is that the subarray take only a middle part, and we know how to find the max subarray sum.
+The second is that the subarray take a part of head array and a part of tail array.
+We can transfer this case to the first one.
+The maximum result equals to the total sum minus the minimum subarray sum.
+
+Here is a diagram by @motorix:
+image
+
+So the max subarray circular sum equals to
+max(the max subarray sum, the total sum - the min subarray sum)
+
+
+Corner case
+Just one to pay attention:
+If all numbers are negative, maxSum = max(A) and minSum = sum(A).
+In this case, max(maxSum, total - minSum) = 0, which means the sum of an empty subarray.
+According to the deacription, We need to return the max(A), instead of sum of am empty subarray.
+So we return the maxSum to handle this corner case.
+
+
+Complexity
+One pass, time O(N)
+No extra space, space O(1)
+ */
 class Solution {
 public:
     int maxSubarraySumCircular(vector<int>& A) {
-		int n = A.size();
-		vector<int> B(A.begin(), A.end());
-		for (int i = 0; i < n; i++)
-		{
-			if (A[i] <= 0) continue;
-			int sum = A[i];
-			for (int j = i + 1; j % n != i; j++)
-			{
-                j = j % n;
-				sum += A[j];
-				B[i] = max(B[i], sum);
-			}
-		}
-		int ret = INT_MIN;
-		for (auto e: B)
-			ret = max(ret, e);
-		return ret;
+        int total = 0, maxSum = -30000, curMax = 0, minSum = 30000, curMin = 0;
+        for (int a : A) {
+            curMax = max(curMax + a, a);
+            maxSum = max(maxSum, curMax);
+            curMin = min(curMin + a, a);
+            minSum = min(minSum, curMin);
+            total += a;
+        }
+        return maxSum > 0 ? max(maxSum, total - minSum) : maxSum;
     }
 };
 int main()
