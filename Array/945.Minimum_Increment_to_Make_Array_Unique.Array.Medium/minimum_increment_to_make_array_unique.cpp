357eb3d82afca5d2845711cef389409922a05582
wisehead <alex.chenhui@gmail.com>
2019-07-03 12:32:34 +0800

    best.

--- Array/945.Minimum_Increment_to_Make_Array_Unique.Array.Medium/minimum_increment_to_make_array_unique.cpp
+++ Array/945.Minimum_Increment_to_Make_Array_Unique.Array.Medium/minimum_increment_to_make_array_unique.cpp
@@ -8,43 +8,18 @@
 #include <iostream>
 #include <vector>
 #include <map>
+#include <set>
 using namespace std;
 class Solution {
 public:
     int minIncrementForUnique(vector<int>& A) {
-		int n = A.size();
-        if (n <= 1) return 0;
-		map<int, int> m;
-		int dups = 0;
-		int sum1 = 0, sum2 = 0;
-		vector<int> vec;
-		vector<int> vec2;
-		for (auto e : A)
-		{
-			if (m.count(e))
-			{
-				m[e]++;
-				sum1 += e;
-                vec.push_back(e);
-			}
-			else
-				m[e] = 1;
-		}
-        int size = vec.size();
-        if (size == 0) return 0;
-        sort(vec.begin(), vec.end());
-		for (int i = 0; i <= vec[size-1] + n; i++)
-		{
-			if (!m.count(i)) 
-				vec2.push_back(i);
-		}
-        for (auto e : vec)
-        {
-			vector<int>::iterator it = upper_bound(vec2.begin(), vec2.end(), e);
-			sum2 += *it;
-			vec2.erase(it);
+        sort(A.begin(), A.end());
+        int res = 0, need = 0;
+        for (int a: A) {
+            res += max(need - a, 0);
+            need = max(a, need)+1;
         }
-		return (sum2 - sum1);
+        return res;
     }
 };
 int main()
