5fa8974c87fdce88aa411ff350cfb27e029b6637
wisehead <alex.chenhui@gmail.com>
2019-07-14 19:50:14 +0800

    better, cleaner

---
+++ Array/1018.Binary_Prefix_Divisible_By_5.Array.Easy/binary_prefix_divisible_by_5.better.cpp
@@ -0,0 +1,26 @@
+/*******************************************************************************
+ *      file name: binary_prefix_divisible_by_5.cpp                                               
+ *         author: Hui Chen. (c) 2019                             
+ *           mail: chenhui13@baidu.com                                        
+ *   created time: 2019/07/14-19:14:34                              
+ *  modified time: 2019/07/14-19:14:34                              
+ *******************************************************************************/
+#include <iostream>
+#include <vector>
+using namespace std;
+class Solution {
+public:
+    vector<bool> prefixesDivBy5(vector<int>& A) {
+		int n = A.size();
+		vector<bool> vec(n, false);
+		int base = 0;
+		for (int i = 0; i < n; i++)
+		{
+			base = (base*2 + A[i]) % 5;
+			if (base == 0) vec[i] = true;
+		}
+		return vec;
+    }
+};
+int main()
+{}
