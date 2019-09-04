89f5f070ec50eedc554e34508e798cda215cd044
wisehead <alex.chenhui@gmail.com>
2019-09-04 11:27:51 +0800

    Easy/938.Range_Sum_of_BST.Tree_Recursion.Easy/

---
+++ Easy/938.Range_Sum_of_BST.Tree_Recursion.Easy/range_sum_of_BST.cpp
@@ -0,0 +1,41 @@
+/*******************************************************************************
+ *      file name: range_sum_of_BST.cpp                                               
+ *         author: Hui Chen. (c) 2019                             
+ *           mail: chenhui13@baidu.com                                        
+ *   created time: 2019/09/04-11:10:08                              
+ *  modified time: 2019/09/04-11:10:08                              
+ *******************************************************************************/
+#include <iostream>
+using namespace std;
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+};
+class Solution {
+public:
+    int rangeSumBST(TreeNode* root, int L, int R) {
+		if (!root) return 0;
+		int sum = 0;
+		if (root->val >= L && root->val <= R)
+			sum += root->val;
+		if (root->left)
+			sum += rangeSumBST(root->left, L, R);
+		if (root->right)
+			sum += rangeSumBST(root->right, L, R);
+		return sum; 
+    }
+};
+
+int main()
+{}
