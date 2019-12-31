cc5274d34de05f29b9449bf191143194f9b7a5fc
wisehead <alex.chenhui@gmail.com>
2019-12-31 22:56:52 +0800

    add print_vec

--- Easy/997.Find_the_Town_Judge.Graph.Easy/find_the_town_judge.cpp
+++ Easy/997.Find_the_Town_Judge.Graph.Easy/find_the_town_judge.cpp
@@ -11,16 +11,27 @@
 using namespace std;
 class Solution {
 public:
+    void print_vec(vector<int>& vec)
+    {
+        vector<int>::iterator it;
+        cout << "the results is:"<< endl;
+        for (it = vec.begin(); it != vec.end(); it++)
+        {
+            cout << *it << " ";
+        }
+        cout<<endl;
+    }
+
     int findJudge(int N, vector<vector<int>>& trust) {
 		vector<int> ss;
 		int index = 0;
+		bool one_round = true;
 		for (int i = 0; i < trust.size(); i++)
 		{
 			if (trust[i][0] == trust[0][0])
 				ss.push_back(trust[i][1]);
 		}
-		for (int i = 0; i < trust.size(); i++)
-		{
+		for (int i = 0; i < trust.size(); i++) {
 			vector<int> s;
 		    vector<int> c;
 			vector<int> vec = trust[i];
@@ -31,11 +42,14 @@ public:
 			}
 			else
 			{
+				one_round = false;
 				index = vec[0];
 				set_intersection(ss.begin(), ss.end(), s.begin(), s.end(), c.begin());
+				print_vec(c);
 				ss.swap(c);
 			}
 		}
+		print_vec(ss);
 		if (ss.size() == 1)
 			return ss[0];
 		return -1;
