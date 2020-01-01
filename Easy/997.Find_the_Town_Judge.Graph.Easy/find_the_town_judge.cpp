c3e66ba7139ca7205963263025a32f600816a0a9
wisehead <alex.chenhui@gmail.com>
2020-01-01 10:03:23 +0800

    map, more clean

--- Easy/997.Find_the_Town_Judge.Graph.Easy/find_the_town_judge.cpp
+++ Easy/997.Find_the_Town_Judge.Graph.Easy/find_the_town_judge.cpp
@@ -19,24 +19,23 @@ public:
 		set<int> ss;
 		set<int> rs;
 		for (int i = 0; i < trust.size(); i++) {
-			vector<int> &vec = trust[i];
-			rs.insert(vec[0]);
-			if (mm.count(vec[1]))
-				mm[vec[1]]++;
-			else
-				mm[vec[1]] = 1;
-			if (mm[vec[1]] == N - 1)
-				ss.insert(vec[1]);
+			int x = trust[i][1];
+			rs.insert(trust[i][0]);
+            
+			if (mm.count(x)) mm[x]++;
+			else mm[x] = 1;
+            
+			if (mm[x] == N - 1)
+				ss.insert(x);
 		}
         
         for (auto e: ss)
-        {
             if (rs.count(e)) ss.erase(e);
-        }
-		if (ss.size() == 1)
-			return *ss.begin();
+        
+		if (ss.size() == 1) return *ss.begin();
 		return -1;
     }
 };
+
 int main()
 {}
