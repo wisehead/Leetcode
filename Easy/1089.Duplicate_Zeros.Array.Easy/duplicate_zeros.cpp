1925e276eeea0cc0644cf23480d380f2a820c89c
wisehead <alex.chenhui@gmail.com>
2020-01-14 20:24:38 +0800

    Easy/1089.Duplicate_Zeros.Array.Easy/

---
+++ Easy/1089.Duplicate_Zeros.Array.Easy/duplicate_zeros.cpp
@@ -0,0 +1,45 @@
+/*******************************************************************************
+ *      file name: duplicate_zeros.cpp                                               
+ *         author: Hui Chen. (c) 2020                             
+ *           mail: chenhui13@baidu.com                                        
+ *   created time: 2020/01/14-20:06:34                              
+ *  modified time: 2020/01/14-20:06:34                              
+ *******************************************************************************/
+#include <iostream>
+#include <vector>
+using namespace std;
+class Solution {
+public:
+    void print_vec(vector<int>& vec)
+    {
+        vector<int>::iterator it;
+        cout << "the results is:"<< endl;
+        for (it = vec.begin(); it != vec.end(); it++)
+        {
+            cout << *it << " ";
+        }
+        cout<<endl;
+    }
+    void duplicateZeros(vector<int>& arr) {
+		vector<int>::iterator it;
+		for (it = arr.begin(); it != arr.end(); it++)
+		{
+			if (*it == 0)
+			{
+				it++;
+				arr.insert(it, 0);
+				arr.pop_back();
+			}
+			print_vec(arr);
+		}
+    }
+};
+int main()
+{
+	int arr[] = {1,0,2,3,0,4,5,0};
+	vector<int> vec(arr,arr+7);
+	Solution sol;
+	sol.print_vec(vec);
+	sol.duplicateZeros(vec);
+	sol.print_vec(vec);
+}
