69023fcd67c9685c96e6bb7d2a6a693806fc1138
wisehead <alex.chenhui@gmail.com>
2020-01-18 13:16:24 +0800

    Array/1053.Previous_Permutation_With_One_Swap.Array_Greedy.Medium not accepted, passed 44/52.

---
+++ Array/1053.Previous_Permutation_With_One_Swap.Array_Greedy.Medium/previous_permutation_with_one_swap.cpp
@@ -0,0 +1,34 @@
+/*******************************************************************************
+ *      file name: previous_permutation_with_one_swap.cpp                                               
+ *         author: Hui Chen. (c) 2020                             
+ *           mail: chenhui13@baidu.com                                        
+ *   created time: 2020/01/18-12:56:45                              
+ *  modified time: 2020/01/18-12:56:45                              
+ *******************************************************************************/
+#include <iostream>
+#include <vector>
+using namespace std;
+class Solution {
+public:
+    vector<int> prevPermOpt1(vector<int>& A) {
+		int n = A.size();
+		for (int i = n - 1; i >= 0; i--)
+		{
+			for (int j = i - 1; j >= 0; j--)
+			{
+				if (A[j] == A[i])
+					break;
+				if (A[j] > A[i])
+				{
+					int tmp = A[i];
+					A[i] = A[j];
+					A[j] = tmp;
+					return A;
+				}
+			}
+		}
+		return A;
+    }
+};
+int main()
+{}
