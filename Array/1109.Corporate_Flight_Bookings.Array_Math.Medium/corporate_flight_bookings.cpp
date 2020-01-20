064c2831429b6792e8d753439aa743c69046fc4f
wisehead <alex.chenhui@gmail.com>
2020-01-20 09:56:46 +0800

    best

--- Array/1109.Corporate_Flight_Bookings.Array_Math.Medium/corporate_flight_bookings.cpp
+++ Array/1109.Corporate_Flight_Bookings.Array_Math.Medium/corporate_flight_bookings.cpp
@@ -8,47 +8,30 @@
 #include <iostream>
 #include <vector>
 using namespace std;
+/*
+Explanation
+Set the change of seats for each day.
+If booking = [i, j, k],
+it needs k more seat on ith day,
+and we don't need these seats on j+1th day.
+We accumulate these changes then we have the result that we want.
+
+
+Complexity
+Time O(booking + N) for one pass on bookings
+Space O(N) for the result
+ */
 class Solution {
 public:
     vector<int> corpFlightBookings(vector<vector<int>>& bookings, int n) {
-		vector<int> vec(n, 0);
-		int size = bookings.size();
-		sort(bookings.begin(), bookings.end());
-		int start = 0;
-		for (int i = 1; i <= n; i++)
-		{
-            //cout<<"--------i:"<<i<<endl;
-			for (int j = start; j < size; j++)
-			{
-                //cout<<"--------j:"<<j<<endl;
-				vector<int> &b = bookings[j];
-                //cout<<"b[0]"<<b[0]<<"   b[1]:"<<b[1]<<" b[2]:"<<b[2]<<endl;
-				if (i < b[0])
-                {
-					break;
-                    //cout<<1<<endl;
-                }
-				if (i == b[0])
-                {
-					vec[i-1] += b[2];
-                    //cout<<2<<endl;
-                }
-				else if (i <= b[1]) //i > j
-                {
-					vec[i-1] += b[2];
-                    //cout<<3<<endl;
-                }
-				else//i > b[1]
-				{
-					if (j == start)
-                        start++;
-                    //cout<<4<<endl;
-				}
-			}
-            //cout<<"vec["<<i-1<<"]"<<vec[i-1]<<endl;
-            
-		}
-		return vec;
+        vector<int> res(n + 1, 0);
+        for (auto & b : bookings) {
+            res[b[0] - 1] += b[2];
+            res[b[1]] -= b[2];
+        }
+        for (int i = 1; i < n; ++i)
+            res[i] += res[i - 1];
+        return {res.begin(), res.end() - 1};
     }
 };
 int main()
