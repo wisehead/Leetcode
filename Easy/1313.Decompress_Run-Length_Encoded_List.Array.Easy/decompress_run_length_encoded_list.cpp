482da8a9ecd0b1e44c45066c15ad247bc8af8a8d
wisehead <alex.chenhui@gmail.com>
2020-01-20 18:17:38 +0800

    Easy/1313.Decompress_Run-Length_Encoded_List.Array.Easy/

---
+++ Easy/1313.Decompress_Run-Length_Encoded_List.Array.Easy/decompress_run_length_encoded_list.cpp
@@ -0,0 +1,27 @@
+/*******************************************************************************
+ *      file name: decompress_run_length_encoded_list.cpp                                               
+ *         author: Hui Chen. (c) 2020                             
+ *           mail: chenhui13@baidu.com                                        
+ *   created time: 2020/01/20-18:12:55                              
+ *  modified time: 2020/01/20-18:12:55                              
+ *******************************************************************************/
+#include <iostream>
+#include <vector>
+using namespace std;
+class Solution {
+public:
+    vector<int> decompressRLElist(vector<int>& nums) {
+		int n = nums.size();
+		vector<int> res;
+		for (int i = 0; i < n; i += 2)
+		{
+			int a = nums[i];
+			int b = nums[i+1];
+			for (int j = 0; j < a; j++)
+				res.push_back(b);
+		}
+		return res;
+    }
+};
+int main()
+{}
