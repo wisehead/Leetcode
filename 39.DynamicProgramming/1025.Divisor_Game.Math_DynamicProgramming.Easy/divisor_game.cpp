c0c374c02a464dc619d43a8203aba7234d82c20e
wisehead <alex.chenhui@gmail.com>
2020-09-20 09:23:34 +0800

    1025.Divisor_Game.Math_DynamicProgramming.Easy

---
+++ 39.DynamicProgramming/1025.Divisor_Game.Math_DynamicProgramming.Easy/divisor_game.cpp
@@ -0,0 +1,17 @@
+/*******************************************************************************
+ *      file name: divisor_game.cpp                                               
+ *         author: Hui Chen. (c) 2020                             
+ *           mail: chenhui13@baidu.com                                        
+ *   created time: 2020/09/20-09:21:53                              
+ *  modified time: 2020/09/20-09:21:53                              
+ *******************************************************************************/
+#include <iostream>
+using namespace std;
+class Solution {
+public:
+    bool divisorGame(int N) {
+        return !(N%2);
+    }
+};
+int main()
+{}
