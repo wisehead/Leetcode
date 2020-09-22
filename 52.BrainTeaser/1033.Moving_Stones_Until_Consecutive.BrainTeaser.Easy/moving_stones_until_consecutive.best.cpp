830e7e2990cdba57fb60e50a2c98a0898fb13c18
wisehead <alex.chenhui@gmail.com>
2020-09-22 09:21:35 +0800

    best

---
+++ 52.BrainTeaser/1033.Moving_Stones_Until_Consecutive.BrainTeaser.Easy/moving_stones_until_consecutive.best.cpp
@@ -0,0 +1,28 @@
+/*******************************************************************************
+ *      file name: moving_stones_until_consecutive.cpp                                               
+ *         author: Hui Chen. (c) 2020                             
+ *           mail: chenhui13@baidu.com                                        
+ *   created time: 2020/09/22-09:15:58                              
+ *  modified time: 2020/09/22-09:15:58                              
+ *******************************************************************************/
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+class Solution {
+public:
+    vector<int> numMovesStones(int a, int b, int c) {
+        vector<int> vec = {a, b, c};
+        sort(vec.begin(), vec.end());
+        int d1 = vec[1] - vec[0] - 1;
+        int d2 = vec[2] - vec[1] - 1;
+        int max_v = d1 + d2;
+        int min_v = min(d1, d2);
+        if ((min_v == 0) && (d1 != d2)) min_v = 1;
+        if (min_v > 2) min_v = 2;
+        vector<int> res = {min_v, max_v};
+        return res;
+    }
+};
+int main()
+{}
