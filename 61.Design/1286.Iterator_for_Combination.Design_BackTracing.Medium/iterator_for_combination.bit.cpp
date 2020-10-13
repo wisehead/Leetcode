d7ac0761d1acf8d5038513d03256141714686864
wisehead <alex.chenhui@gmail.com>
2020-10-13 14:58:28 +0800

    bit

---
+++ 61.Design/1286.Iterator_for_Combination.Design_BackTracing.Medium/iterator_for_combination.bit.cpp
@@ -0,0 +1,52 @@
+/*******************************************************************************
+ *      file name: iterator_for_combination.cpp                                               
+ *         author: Hui Chen. (c) 2020                             
+ *           mail: chenhui13@baidu.com                                        
+ *   created time: 2020/10/13-09:35:32                              
+ *  modified time: 2020/10/13-09:35:32                              
+ *******************************************************************************/
+#include <iostream>
+#include <vector>
+#include <set>
+using namespace std;
+set<string>GenaretallComb(string s,int len){
+    int mask = 1<<s.length();
+    set<string>hold;
+    string comString = "";
+    for(int no=1;no<mask;no++){
+        int num = no,i = 0;
+        while(num){
+            if(num&1)comString = comString + s[i];
+            i++,num>>=1;
+        }
+        if(comString.length()==len)hold.insert(comString);
+        comString = "";
+    }
+    return hold;
+}
+
+class CombinationIterator {
+public:
+    set<string>st;
+    set <string> :: iterator cur;
+    CombinationIterator(string characters, int combinationLength) {
+        this->st = GenaretallComb(characters,combinationLength);
+        this->cur = begin(st);
+    }
+    
+    string next() {
+        return cur==end(st)?"":*cur++;
+    }
+    
+    bool hasNext() {
+        return cur!=end(st);
+    }
+};
+/**
+ * Your CombinationIterator object will be instantiated and called as such:
+ * CombinationIterator* obj = new CombinationIterator(characters, combinationLength);
+ * string param_1 = obj->next();
+ * bool param_2 = obj->hasNext();
+ */
+int main()
+{}
