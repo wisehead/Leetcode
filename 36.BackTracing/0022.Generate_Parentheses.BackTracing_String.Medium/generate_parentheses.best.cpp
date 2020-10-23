80294bc6dc35a346d0a26751f580d1f61d451293
wisehead <alex.chenhui@gmail.com>
2020-10-23 14:16:33 +0800

    best

---
+++ 36.BackTracing/0022.Generate_Parentheses.BackTracing_String.Medium/generate_parentheses.best.cpp
@@ -0,0 +1,29 @@
+/*******************************************************************************
+ *      file name: generate_parentheses.cpp                                               
+ *         author: Hui Chen. (c) 2020                             
+ *           mail: chenhui13@baidu.com                                        
+ *   created time: 2020/10/23-14:01:47                              
+ *  modified time: 2020/10/23-14:01:47                              
+ *******************************************************************************/
+#include <iostream>
+#include <vector>
+#include <stack>
+using namespace std;
+class Solution {
+public:
+    vector<string> generateParenthesis(int n) {
+        vector<string> res;
+        addingpar(res, "", n, 0);
+        return res;
+    }
+    void addingpar(vector<string> &v, string str, int n, int m){
+        if(n==0 && m==0) {
+            v.push_back(str);
+            return;
+        }
+        if(m > 0){ addingpar(v, str+")", n, m-1); }
+        if(n > 0){ addingpar(v, str+"(", n-1, m+1); }
+    }
+};
+int main()
+{}
