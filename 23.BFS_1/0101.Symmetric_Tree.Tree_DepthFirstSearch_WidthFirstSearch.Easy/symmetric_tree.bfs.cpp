4a1593de417aaeda041d2e69e9354eab38a83750
wisehead <alex.chenhui@gmail.com>
2020-11-01 09:31:40 +0800

    BFS

---
+++ 23.BFS_1/0101.Symmetric_Tree.Tree_DepthFirstSearch_WidthFirstSearch.Easy/symmetric_tree.bfs.cpp
@@ -0,0 +1,50 @@
+/*******************************************************************************
+ *      file name: symmetric_tree.cpp                                               
+ *         author: hui chen. (c) 17                             
+ *           mail: chenhui13@baidu.com                                        
+ *   created time: 17/11/08-11:11                                    
+ * modified time: 17/11/08-11:11                                    
+ *******************************************************************************/
+#include <iostream>
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+ struct TreeNode {
+     int val;
+     TreeNode *left;
+     TreeNode *right;
+     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ };
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    bool isSymmetric(TreeNode* root) {
+        if (!root) return true;
+        return bfs(root->left, root->right);
+        
+    }
+    bool bfs(TreeNode* root1, TreeNode* root2) {
+        if (!root1 && !root2) return true;
+        if (!root1 || !root2) return false;
+        if (root1->val != root2->val) return false;
+        return bfs(root1->left, root2->right) && bfs(root1->right, root2->left);
+    }
+};
+int main()
+{}
