1e5fa858300c5a60e04447e5acbdf5154588a0dd
wisehead <alex.chenhui@gmail.com>
2021-08-15 09:22:23 +0800

    18

---
+++ 18.BitMask/1.cpp
@@ -0,0 +1,13 @@
+/*******************************************************************************
+ *      file name: 1.cpp                                               
+ *         author: Hui Chen. (c) 2021                             
+ *           mail: alex.chenhui@gmail.com                                        
+ *   created time: 2021/08/15-09:22:01                              
+ *  modified time: 2021/08/15-09:22:01                              
+ *******************************************************************************/
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int main()
+{}
