20b06fe01bc0ad9a2d6a779806bc5f428d1b67cc
wisehead <alex.chenhui@gmail.com>
2021-10-05 18:52:39 +0800

    queue

---
+++ 23.BFS/0116.Populating_Next_Right_Pointers_in_Each_Node.Tree_DepthFirstSearch_BreadthFirstSearch_BinaryTree.Medium/populating_next_right_pointers_in_each_node.queue.cpp
@@ -0,0 +1,49 @@
+/*******************************************************************************
+ *      file name: populating_next_right_pointers_in_each_node.cpp                                               
+ *         author: Hui Chen. (c) 2021                             
+ *           mail: alex.chenhui@gmail.com                                        
+ *   created time: 2021/10/05-18:50:10                              
+ *  modified time: 2021/10/05-18:50:10                              
+ *******************************************************************************/
+#include <iostream>
+#include <vector>
+#include <queue>
+using namespace std;
+// Definition for a Node.
+class Node {
+public:
+    int val;
+    Node* left;
+    Node* right;
+    Node* next;
+
+    Node() : val(0), left(NULL), right(NULL), next(NULL) {}
+
+    Node(int _val) : val(_val), left(NULL), right(NULL), next(NULL) {}
+
+    Node(int _val, Node* _left, Node* _right, Node* _next)
+        : val(_val), left(_left), right(_right), next(_next) {}
+};
+class Solution {
+public:
+    Node* connect(Node* root) {
+        if (!root) return root;
+        queue<Node*> q;
+        queue<Node*> q2;
+        q.push(root);
+        while (!q.empty()) {
+            while (!q.empty()) {
+                Node* node = q.front();
+                q.pop();
+                if (!q.empty())
+                    node->next = q.front();
+                if (node->left) q2.push(node->left);
+                if (node->right) q2.push(node->right);
+            }
+            swap(q,q2);
+        }
+        return root;
+    }
+};
+int main()
+{}
