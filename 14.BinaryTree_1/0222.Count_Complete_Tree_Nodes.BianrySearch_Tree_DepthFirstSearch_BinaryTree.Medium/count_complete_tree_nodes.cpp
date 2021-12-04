050479b47a67f7bf883781621f5ca9a900d09012
wisehead <alex.chenhui@gmail.com>
2021-12-04 09:32:12 +0800

    DFS

---
+++ 14.BinaryTree_1/0222.Count_Complete_Tree_Nodes.BianrySearch_Tree_DepthFirstSearch_BinaryTree.Medium/count_complete_tree_nodes.cpp
@@ -0,0 +1,41 @@
+/*******************************************************************************
+ *      file name: count_complete_tree_nodes.cpp                                               
+ *         author: Hui Chen. (c) 2021                             
+ *           mail: alex.chenhui@gmail.com                                        
+ *   created time: 2021/12/04-09:30:52                              
+ *  modified time: 2021/12/04-09:30:52                              
+ *******************************************************************************/
+#include <iostream>
+#include <vector>
+using namespace std;
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+};
+class Solution {
+public:
+    int countNodes(TreeNode* root) {
+        if (!root) return 0;
+        int sum = 1;
+        sum += countNodes(root->left);
+        sum += countNodes(root->right);
+        return sum;        
+    }
+};
+int main()
+{}
