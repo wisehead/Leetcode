49406b63bdeb9a7c59c31261f64e28c26043956b
wisehead <alex.chenhui@gmail.com>
2021-12-12 07:40:06 +0800

    best with comments.

--- 31.Recursion_1/0779.K-th_Symbol_in_Grammar.Math_BitManipulation_Recursion.Medium/k-th_symbol_in_grammer.cpp
+++ 31.Recursion_1/0779.K-th_Symbol_in_Grammar.Math_BitManipulation_Recursion.Medium/k-th_symbol_in_grammer.cpp
@@ -8,57 +8,28 @@
 #include <iostream>
 #include <vector>
 using namespace std;
+/*
+Updates: (all index discussed below are 0-based)
+Observation 1: N does not matter as long as "K will be an integer in the range [1, 2^(N-1)]". We can ignore N.
+
+Observation 2: let f(k) be the value of kth position (0-based), then:
+f(2 * k) = 0 {if f(k) = 0} or, 1 {if f(k) = 1} => f(2 * k) = f(k) xor 0
+f(2 * k + 1) = 0 {if f(k) = 1} or 1 {if f(k) = 0} => f(2 * k + 1) = f(k) xor 1
+
+Obervation 3: if binary string of k is used, let k = 1001010, then we have:
+f(1001010) = f(100101) ^ 0 = f(10010) ^ 1 ^ 0 = f(1001) ^ 0 ^ 1 ^ 0 = ... = f(0) ^ 1 ^ 0 ^ 0 ^1 ^ 0 ^ 1 ^ 0 = 1 ^ 0 ^ 0 ^1 ^ 0 ^ 1 ^ 0
+So, the result is the xor operation on all bits of k. Since 0 does not change xor result, we can ignore all 0s.
+f(1001010) = 1 ^ 1 ^ 1 = (1^1) ^ 1 = 0 ^ 1 = 1
+f(11110011) = 1 ^ 1^ 1 ^ 1 ^ 1 ^1 = (1 ^ 1) ^ (1 ^ 1) ^ (1 ^1) = 0
+Now, it's easy to tell f(k) = 0 if k has even number of 1s in binary representation, and f(k) = 1 when k has odd number of 1s
+*/
 class Solution {
 public:
-    int get_n_bit(vector<char>& vec, int n, int k) {
-        //cout<<"n:"<<n<<",k:"<<k<<endl;
-        unsigned long long int masks[] = {1,2,4,8,16,32,64,128};
-        int index = (k-1) / 8;
-        k = k % 8;
-        int offset = (n-k)%8;
-        //cout<<"index:"<<index<<",offset:"<<offset<<endl;
-        int v = vec[index];
-        if (v & masks[offset]) return 1;
-        return 0;
+    int kthGrammar(int N, int K) {
+        int n;
+        for (n = 0, K -= 1; K ; K &= (K - 1)) n++;
+        return n & 1;
     }
-    int kthGrammar(int n, int k) {
-        vector<char> vec; 
-        int N = 0;
-            if (n == 1) {
-                vec.push_back(0);
-                N = 1;
-            } else if (n == 2) {
-                vec.push_back(1);
-                N = 2;
-            }
-            else if (n==3) {
-                vec.push_back(6);
-                N = 4;
-            }
-            else {
-                vec.push_back(105);
-                N = 8;
-            }
-            
-    
-	    
-	    for (int level = 5; level <= n; level++) {
-		    vector<char> v2;
-		    for (auto c : vec) {
-			    char x = c ^ 0b11111111;
-			    v2.push_back(x);
-                
-                if ((v2.size()+ vec.size())*8 >= k) {
-                    vec.insert(vec.end(), v2.begin(), v2.end());
-                    return get_n_bit(vec, N, k); 
-                }
-                
-		    }
-		    vec.insert(vec.end(), v2.begin(), v2.end());
-            //if (vec.size() >= k) break;
-	    }
-        return get_n_bit(vec, N, k); 
-    }
-    
+
     //string f(string )
 };
