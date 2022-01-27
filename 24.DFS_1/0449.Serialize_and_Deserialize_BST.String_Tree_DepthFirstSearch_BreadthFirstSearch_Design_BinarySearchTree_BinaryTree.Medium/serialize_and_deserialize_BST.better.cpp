f8b754dc685cad2f1cc4a4d423ba70e067c5cc73
wisehead <alex.chenhui@gmail.com>
2022-01-27 09:56:02 +0800

    preorder + max min value.

---
+++ 24.DFS_1/0449.Serialize_and_Deserialize_BST.String_Tree_DepthFirstSearch_BreadthFirstSearch_Design_BinarySearchTree_BinaryTree.Medium/serialize_and_deserialize_BST.better.cpp
@@ -0,0 +1,89 @@
+/*******************************************************************************
+ *      file name: serialize_and_deserialize_BST.cpp                                               
+ *         author: Hui Chen. (c) 2022                             
+ *           mail: alex.chenhui@gmail.com                                        
+ *   created time: 2022/01/26-22:34:55                              
+ *  modified time: 2022/01/26-22:34:55                              
+ *******************************************************************************/
+#include <iostream>
+#include <vector>
+using namespace std;
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+};
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+class Codec {
+public:
+
+    // Encodes a tree to a single string.
+    string serialize(TreeNode* root) {
+        string order;
+        inorderDFS(root, order);
+        return order;
+    }
+
+    inline void inorderDFS(TreeNode* root, string& order) {
+        if (!root) return;
+        char buf[4];
+        memcpy(buf, &(root->val), sizeof(int)); //burn the int into 4 chars
+        for (int i=0; i<4; i++) order.push_back(buf[i]);
+        inorderDFS(root->left, order);
+        inorderDFS(root->right, order);
+    }
+
+    // Decodes your encoded data to tree.
+    TreeNode* deserialize(string data) {
+        int pos = 0;
+        return reconstruct(data, pos, INT_MIN, INT_MAX);
+    }
+
+    inline TreeNode* reconstruct(const string& buffer, int& pos, int minValue, int maxValue) {
+        if (pos >= buffer.size()) return NULL; //using pos to check whether buffer ends is better than using char* directly.
+
+        int value;
+        memcpy(&value, &buffer[pos], sizeof(int));
+        if (value < minValue || value > maxValue) return NULL;
+
+        TreeNode* node = new TreeNode(value);
+        pos += sizeof(int);
+        node->left = reconstruct(buffer, pos, minValue, value);
+        node->right = reconstruct(buffer, pos, value, maxValue);
+        return node;
+    }
+};
+
+// Your Codec object will be instantiated and called as such:
+// Codec* ser = new Codec();
+// Codec* deser = new Codec();
+// string tree = ser->serialize(root);
+// TreeNode* ans = deser->deserialize(tree);
+// return ans;
+// Your Codec object will be instantiated and called as such:
+// Codec* ser = new Codec();
+// Codec* deser = new Codec();
+// string tree = ser->serialize(root);
+// TreeNode* ans = deser->deserialize(tree);
+// return ans;
+int main()
+{}
