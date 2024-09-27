5294e1f66300fa3ecddb5f0ddad42973809062d1
Hui Chen <alex.chenhui@gmail.com>
2024-09-27 15:23:02 +0800

    BinaryTree + 1

---
+++ 14.BinaryTree_2/2236.Root_Equals_Sum_of_Children.Tree_BinaryTree.Easy/root_equals_sum_of_children.cpp
@@ -0,0 +1,25 @@
+/*******************************************************************************
+ *      file name: root_equals_sum_of_children.cpp                                               
+ *         author: hui chen. (c) 24                             
+ *           mail: alex.chenhui@gmail.com                                        
+ *   created time: 24/09/27- 9:09                                    
+ * modified time: 24/09/27- 9:09                                    
+ *******************************************************************************/
+#include <iostream>
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    bool checkTree(TreeNode* root) {
+        return (root->val == root->left->val + root->right->val);
+    }
+};
