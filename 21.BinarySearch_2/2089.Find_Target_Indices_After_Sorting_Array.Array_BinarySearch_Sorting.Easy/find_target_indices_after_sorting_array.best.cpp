7bb1ddb0f5077bb5a9825082686e1f9a5be3361f
Hui Chen <alex.chenhui@gmail.com>
2024-09-27 09:59:05 +0800

    recusive

---
+++ 21.BinarySearch_2/2089.Find_Target_Indices_After_Sorting_Array.Array_BinarySearch_Sorting.Easy/find_target_indices_after_sorting_array.best.cpp
@@ -0,0 +1,35 @@
+/*******************************************************************************
+ *      file name: find_target_indices_after_sorting_array.cpp                                               
+ *         author: hui chen. (c) 24                             
+ *           mail: alex.chenhui@gmail.com                                        
+ *   created time: 24/09/27- 9:09                                    
+ * modified time: 24/09/27- 9:09                                    
+ *******************************************************************************/
+#include <iostream>
+class Solution {
+public:
+    vector <int> ans;   //to store answers
+    vector<int> targetIndices(vector<int>& nums, int target) {
+        sort(nums.begin(), nums.end());     //sorting arrays initially
+        int start=0, end=nums.size()-1;
+        binary(nums, start, end, target);   //calling binary search on whole array
+        return ans;
+    }
+
+    void binary(vector <int> nums, int start, int end, int target){
+        if(start>end)       //if array can't be divided into subarrays anymore
+            return;
+
+        int mid = start + (end-start)/2;        //declaring mid element
+        if(nums[mid] == target){                //when target is found, push it into ans and call binary on each halved subarrays
+            binary(nums, start, mid-1, target);
+            ans.push_back(mid);
+            binary(nums, mid+1, end, target);
+        }
+        else if(nums[mid]>target)               //calling binary on lower half of array
+            binary(nums, start, mid-1, target);
+        else                                    //calling binary on upper half of array
+            binary(nums, mid+1, end, target);
+    }
+
+};
