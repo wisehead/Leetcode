fbe99a0907f56ee83baa3569a02feec7c2602546
Hui Chen <alex.chenhui@gmail.com>
2025-01-17 14:36:16 +0800

    0877.Stone_Game.GameTheory.Medium

---
+++ 73.GameTheory_3/0877.Stone_Game.GameTheory.Medium/stone_game.cpp
@@ -0,0 +1,14 @@
+/*******************************************************************************
+ *      file name: stone_game.cpp                                               
+ *         author: hui chen. (c) 25                             
+ *           mail: alex.chenhui@gmail.com                                        
+ *   created time: 25/01/17- 1:01                                    
+ * modified time: 25/01/17- 1:01                                    
+ *******************************************************************************/
+#include <iostream>
+class Solution {
+public:
+    bool stoneGame(vector<int>& piles) {
+        return true;
+    }
+};
